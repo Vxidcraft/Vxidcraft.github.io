d7776c0779f85d32c282514e5e461feb97fba2a9
Cédric <cedbossant@gmail.com>
2024-09-02 12:18:22 +0200

    mise en ligne

---
+++ index.js
@@ -0,0 +1,29 @@
+const generateA = document.getElementById("generateA");
+const generateH = document.getElementById("generateH");
+
+const btnAlly = document.querySelector(".btn-ally");
+const btnHorde = document.querySelector(".btn-horde");
+
+const raceOutput = document.getElementById("race-output");
+const classOutput = document.getElementById("class-output");
+
+let AArace = ["Humain", "EDN", "Nain"];
+let AAclass = ["Guerrier", "mage", "Voleur", "Pretre", "Démoniste"];
+
+let AHrace = ["Orc", "Mort-Vivant", "Troll"];
+let AHclass = ["Guerrier", "mage", "Voleur", "Pretre", "Démoniste"];
+
+const randAAR = Math.floor(Math.random() * AArace.length);
+const randAAC = Math.floor(Math.random() * AAclass.length);
+const randHAR = Math.floor(Math.random() * AHrace.length);
+const randHAC = Math.floor(Math.random() * AHclass.length);
+
+btnAlly.addEventListener("click", () => {
+  raceOutput.value = AArace[randAAR];
+  classOutput.value = AAclass[randAAC];
+});
+
+btnHorde.addEventListener("click", () => {
+  raceOutput.value = AHrace[randHAR];
+  classOutput.value = AHclass[randHAC];
+});
